ce8d11f69cde4a3992d192be26cf6bcc32596193
asdChenJin <33625968+asdChenJin@users.noreply.github.com>
2024-09-29 14:27:37 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=yHL6s39qUs3yJHKiQgBIna5; device_id=192667104317200849332206928eb5cb12e51d7367093351865b5720b1; sess=BA-05jxEFTQzcbOZm1wDH%2FhGAAbuao6F7AuAqH6jswfdo1tAWzF9u8DXyutGhaSvu1sHlLcM6FxFAcP4rNsO7EE1EMMXlPApAKi4Yt3ur7TnCyXEKNEhTNiTWKu; user=user%3A8980555784%7C8980555784; smzdm_id=8980555784; homepage_sug=c; r_sort_type=score; footer_floating_layer=0; ad_date=29; bannerCounter=%5B%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; ad_json_feed=%7B%7D; _zdmA.uid=ZDMA.yvQCTUQz8.1727591005.2419200; _zdmA.vid=*; _zdmA.time=1727591112212.0.https%3A%2F%2Fwww.smzdm.com%2F'
 
 
 """
